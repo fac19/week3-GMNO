27cd927fb52688fc264a719d0281bbf0aababaa6
James-Hardy <jamesjhardy95@gmail.com>
2020-03-18 16:58:04 +0000

    fix js functions

--- script.js
+++ script.js
@@ -5,49 +5,35 @@ const cardForm = document.querySelector(".add-card");
 const cardTemplate = document.querySelector("#todo-card");
 
 
-function createCard(){
-const domFragment = cardTemplate.content.cloneNode(true);
-domFragment.querySelector(".card__title").innerHTML = cardTitle.value;
-domFragment.querySelector(".card_remove-button").addEventListener('click', function (e) {
-this.parentNode.remove();
-});
-
-
-cardContainer.appendChild(domFragment);
-
+function createCard() {
+    const domFragment = cardTemplate.content.cloneNode(true);
+    domFragment.querySelector(".card__title").innerHTML = cardTitle.value;
+    domFragment.querySelector(".card_remove-button").addEventListener('click', function (e) {
+        this.parentNode.remove();
+    });
+    // addListItem();
+    cardContainer.appendChild(domFragment);
 }
-
 
 addCardButton.addEventListener('click', createCard);
-//const submitBtn 
-const userInput = document.querySelector("#userToDo");
-const addButton = document.querySelectorAll("#addListBtn");
-let cardList = document.querySelector("#cardList");
 
-/*Need to check if these are live or static nodes*/
 
-function catchUserInput () {
-    return userInput.value;
-}
 
-addButton.addEventListener("click", function() {
-    catchUserInput();
-    console.log(catchUserInput());
-})
 
-//const submitBtn 
-const userInput = document.querySelector("#userToDo");
-const addButton = document.querySelectorAll("#addListBtn");
-let cardList = document.querySelector("#cardList");
+// function addListItem(){
+//     //const submitBtn 
+//     const userInput = document.querySelector("#userToDo");
+//     const addButton = document.querySelectorAll("#addListBtn");
+// let cardList = document.querySelector("#cardList");
 
-/*Need to check if these are live or static nodes*/
+// /*Need to check if these are live or static nodes*/
 
-function catchUserInput () {
-    return userInput.value;
-}
+// function catchUserInput () {
+//     return userInput.value;
+// }
 
-addButton.addEventListener("click", function() {
-    catchUserInput();
-    console.log(catchUserInput());
-})
-
+// addButton.addEventListener("click", function() {
+//     catchUserInput();
+//     console.log(catchUserInput());
+// })
+// }
