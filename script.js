e0954322607389ce60a36b21ff0a37baa751817c
Ina Yoon <itsina96@gmail.com>
2020-03-18 16:17:21 +0000

    add js objects

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //const submitBtn 
+const userInput = document.querySelector("#userToDo");
+const addButton = document.querySelectorAll("#addListBtn");
+let cardList = document.querySelector("#cardList");
+
+/*Need to check if these are live or static nodes*/
+
+function catchUserInput () {
+    return userInput.value;
+}
+
+addButton.addEventListener("click", function() {
+    catchUserInput();
+    console.log(catchUserInput());
+})
+
