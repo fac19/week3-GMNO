ee46f901b997e76c4cccc48f75bd83977f660566
Vatsal Kanoria <vatskan@gmail.com>
2020-03-18 23:13:05 +0000

    Refactoring and bug fix for card__add-li-button

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
-const addCardButton = document.querySelector(".add-card__button");
-const cardTitle = document.querySelector(".add-card__title");
-const cardContainer = document.querySelector(".card-container");
-const cardForm = document.querySelector(".add-card");
-const cardTemplate = document.querySelector("#toDoCard");
+// const addCardButton = document.querySelector(".add-card__button");
+// const cardTitle = document.querySelector(".add-card__title");
+// const cardContainer = document.querySelector(".card-container");
+// const cardForm = document.querySelector(".add-card");
+// const cardTemplate = document.querySelector("#toDoCard");
 
 
-function createCard() {
-    const domFragment = cardTemplate.content.cloneNode(true);
-    domFragment.querySelector(".card__title").innerHTML = cardTitle.value;
-    domFragment.querySelector(".card__remove-button").addEventListener('click', function (e) {
-        this.parentNode.remove();
-    });
-    // addListItem();
-    cardContainer.appendChild(domFragment);
-}
+// function createCard() {
+//     const domFragment = cardTemplate.content.cloneNode(true);
+//     domFragment.querySelector(".card__title").textContent = cardTitle.value;
+//     domFragment.querySelector(".card__remove-button").addEventListener('click', function () {
+//         this.parentNode.remove();
+//     });
+//     cardContainer.appendChild(domFragment);
+//     addListItem();
 
-addCardButton.addEventListener('click', createCard);
-
-
-
-
-// function addListItem(){
-//     //const submitBtn 
-//     const userInput = document.querySelector("#userToDo");
-//     const addButton = document.querySelectorAll("#addListBtn");
-//     let cardList = document.querySelector("#cardList");
-
-//     /*Need to check if these are live or static nodes*/
-
-//     function catchUserInput () {
-//         return userInput.value;
-//     }
-
-//     addButton.addEventListener("click", function() {
-//         catchUserInput();
-//         console.log(catchUserInput());
-//     })
 // }
-// addListItem(); //to test 
+// addCardButton.addEventListener('click', createCard);
+
+
+function addListItem(){
+    const userInput = document.querySelector(".card__user-input");
+    const addButton = document.querySelector(".card__add-li-button");
+    const listTemplate = document.querySelector("#cardListItem")
+    //let cardList = document.querySelector(".card__list");
+    //Need to check if these are live or static nodes
+    //console.log(addButton);
+    addButton.addEventListener("click", () => {
+        
+    });
+}
+addListItem(); //to test 
