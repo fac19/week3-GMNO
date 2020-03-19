d84834048e1914c2a87229730c5ebb184e23310e
Vatsal Kanoria <vatskan@gmail.com>
2020-03-19 00:12:28 +0000

    try to make the two add buttons work together, known bug

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-// const addCardButton = document.querySelector(".add-card__button");
-// const cardTitle = document.querySelector(".add-card__title");
-// const cardContainer = document.querySelector(".card-container");
-// const cardForm = document.querySelector(".add-card");
-// const cardTemplate = document.querySelector("#toDoCard");
+const addCardButton = document.querySelector(".add-card__button");
+const cardTitle = document.querySelector(".add-card__title");
+const cardContainer = document.querySelector(".card-container");
+const cardForm = document.querySelector(".add-card");
+const cardTemplate = document.querySelector("#toDoCard");
 
 
-// function createCard() {
-//     const domFragment = cardTemplate.content.cloneNode(true);
-//     domFragment.querySelector(".card__title").textContent = cardTitle.value;
-//     domFragment.querySelector(".card__remove-button").addEventListener('click', function () {
-//         this.parentNode.remove();
-//     });
-//     cardContainer.appendChild(domFragment);
-//     addListItem();
+function createCard() {
+    const domFragment = cardTemplate.content.cloneNode(true);
+    domFragment.querySelector(".card__title").textContent = cardTitle.value;
+    domFragment.querySelector(".card__remove-button").addEventListener('click', function() {
+        this.parentNode.remove();
+    }); //why can't this be defined as an inline function?--not working as inline
+    cardContainer.appendChild(domFragment);
+    addListItem();
+}
+addCardButton.addEventListener('click', createCard);
 
-// }
-// addCardButton.addEventListener('click', createCard);
 
-
-function addListItem(){
+function addListItem(){  
+    //need to make this relevant to the particular dom fragment
+    //i.e. getting bug when multiple cards (several inputs (multiple of number of cards) appear)
     const addListItemButton = document.querySelector(".card__add-li-button");
     const listTemplate = document.querySelector("#cardListItemTemplate");
     const listInput = document.querySelector(".card__user-input");
     const cardList = document.querySelector(".card__list")
-    //let cardList = document.querySelector(".card__list");
-    //Need to check if these are live or static nodes
-    //console.log(addButton);
+
     addListItemButton.addEventListener("click", () => {
         const listFragment = listTemplate.content.cloneNode(true); 
         listFragment.querySelector(".card__list-item").textContent = listInput.value;
@@ -33,7 +32,7 @@ function addListItem(){
         console.log(listFragment);
     });
 }
-addListItem(); //to test 
+//addListItem(); //to test 
 
 function removeListItem(){
     const removeListItemButton = document.querySelector(".card__list-remove-button")
