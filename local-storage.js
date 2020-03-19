567223a458d97d2d3217ef0690ecbf8b584b3e31
Vatsal Kanoria <vatskan@gmail.com>
2020-03-19 23:23:22 +0000

    basic info on local storage

---
+++ local-storage.js
@@ -0,0 +1,11 @@
+// if(localStorage){
+//     localStorage.setItem("name", "Peter");
+//     alert(localStorage.getItem("name"));
+//     localStorage.removeItem("name");
+// }else{
+//     alert("Your browser does not support local storage")
+// }
+
+// function storeCard() {
+
+// }
