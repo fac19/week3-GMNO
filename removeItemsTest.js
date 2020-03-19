151ba7bb80005cea2ad0a39ef54697dedda4f136
Alexreid95 <alexreid95@gmail.com>
2020-03-19 12:32:45 +0000

    Wrote first test, not yet completed
    Co-authered-by: Vatsal <vatskan@gmail.com>

--- removeItemsTest.js
+++ removeItemsTest.js
@@ -17,7 +17,22 @@ function test(nameOfTest, testFunction) {
     console.groupEnd(nameOfTest);
 }
 
-
+// Testing to the test
 test("testing", t => {
     t.equal(2, 3);
+})
+
+
+
+test("Should check if there is 1 list item in card__list", t => {
+    //This test is currently not updating dynamically (with respect to the add-list-item-button)
+    //Therefore it is always a fail test or always a pass test
+    //Therefore it currently does not work. 
+
+    const expected = 0; //
+    //console.log(document.querySelector(".card__list").childNodes);
+    const result = document.querySelector(".card__list").childNodes.length;
+    // console.log(result);
+    t.equal(result, expected)
+
 })
--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-const addCardButton = document.querySelector(".add-card__button");
-const cardTitle = document.querySelector(".add-card__title");
-const cardContainer = document.querySelector(".card-container");
-const cardForm = document.querySelector(".add-card");
-const cardTemplate = document.querySelector("#toDoCard");
+// const addCardButton = document.querySelector(".add-card__button");
+// const cardTitle = document.querySelector(".add-card__title");
+// const cardContainer = document.querySelector(".card-container");
+// const cardForm = document.querySelector(".add-card");
+// const cardTemplate = document.querySelector("#toDoCard");
+
+// function createCard() {
+//     const cardFragment = cardTemplate.content.cloneNode(true);
+//     cardFragment.querySelector(".card__title").textContent = cardTitle.value;
+//     cardFragment.querySelector(".card__remove-button").addEventListener('click', function () {
+//         this.parentNode.remove();
+//     }); //why can't this be defined as an inline function?--not working as inline
+//     cardContainer.appendChild(cardFragment);
+//     addListItem();
+// }
+// addCardButton.addEventListener('click', createCard);
 
 
-function createCard() {
-    const cardFragment = cardTemplate.content.cloneNode(true);
-    cardFragment.querySelector(".card__title").textContent = cardTitle.value;
-    cardFragment.querySelector(".card__remove-button").addEventListener('click', function() {
-        this.parentNode.remove();
-    }); //why can't this be defined as an inline function?--not working as inline
-    cardContainer.appendChild(cardFragment);
-    addListItem();
-}
-addCardButton.addEventListener('click', createCard);
-
-
-function addListItem(){  
+function addListItem() {
     //need to make this relevant to the particular dom fragment
     //i.e. getting bug when multiple cards (several inputs (multiple of number of cards) appear)
     const addListItemButton = document.querySelector(".card__list-add-button");
     const listTemplate = document.querySelector("#cardListItemTemplate");
     const listInput = document.querySelector(".card__user-input");
-    const cardList = document.querySelector(".card__list")
+    const cardList = document.querySelector(".card__list");
 
     addListItemButton.addEventListener("click", () => {
-        const listFragment = listTemplate.content.cloneNode(true); 
+        const listFragment = listTemplate.content.cloneNode(true);
         listFragment.querySelector(".card__list-item").textContent = listInput.value;
         cardList.appendChild(listFragment);
         //console.log(listFragment);
     });
 }
-//addListItem(); //to test 
+addListItem(); //to test 
 
-function removeListItem(){
+function removeListItem() {
     const removeListItemButton = document.querySelector(".card__list-remove-button")
     console.log(removeListItemButton);
     //write a function which removes a list item when a button is clicked.
+
 }
